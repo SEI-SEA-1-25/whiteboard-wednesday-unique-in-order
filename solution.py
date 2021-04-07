eaa96f0de8996b0e4a95890245c950c082071665
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-04-07 09:13:14 -0700

    vest

--- solution.py
+++ solution.py
@@ -3,9 +3,10 @@ def unique_in_order(str_or_arr):
     for i in range(len(str_or_arr)):
         j = i - 1
         if str_or_arr[j] != str_or_arr[j + 1]:
-            print(str_or_arr[i])
+            # print(str_or_arr[i])
             new_list.append(str_or_arr[i])
-            print(new_list)
+    return(new_list)
 
 
-unique_in_order('AAAABBBCCDAABBB')
+
+unique_in_order([1,2,2,3,3])
